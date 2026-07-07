0ee73cc067df031309482f321b4e6ca3b7679ba3
HR-DASHBOARD <alessarena27@gmail.com>
2026-07-07 23:18:07 +0200

    Fix current frontend CORS domain

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -12,7 +12,7 @@ return [
     ],
 
 	'allowed_origins' => [
-		'https://hr-dashboard-a1ly23x07-aleisdiffs-projects.vercel.app',
+		'https://hr-dashboard-nu-beryl.vercel.app',
 		env('FRONTEND_URL'),
 		'http://localhost:5173',
 		'http://127.0.0.1:5173',
